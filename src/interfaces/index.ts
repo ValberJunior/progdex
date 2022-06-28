6925fe3dab4251d20c8b42b6bc458684b094c152
ValberJunior <valberjr1992@gmail.com>
2022-06-28 06:36:40 -0300

    start the project

---
+++ src/interfaces/index.ts
@@ -0,0 +1,14 @@
+export interface ProgrammingLanguage {
+    "language": number,
+    "Intended use": string,
+    "Imperative": string,
+    "Object-oriented": string,
+    "Functional": string | null,
+    "Procedural": string,
+    "Generic": string,
+    "Reflective": string,
+    "Event-driven": string,
+    "Other paradigm(s)": string,
+    "Standardized?": string
+  }
+  
