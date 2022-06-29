b771b0726fbafe2aa0c50032b80608f3205454a0
ValberJunior <valberjr1992@gmail.com>
2022-06-29 14:49:52 -0300

    create components default

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,21 +1,12 @@
-import { CardList, Header } from 'components'
-import "./_home.scss";
-import classNames from 'classnames';
-import { themeModeEnum } from 'utils/enum/theme-mode-enum';
-import { useThemeMode } from 'state/hooks/useThemeMode';
+import { CardList } from 'components'
+import "./_Home.scss";
 
 const Home = () => {
 
-  const theme : themeModeEnum = useThemeMode(); 
-  
   return (
-    <main className={classNames({
-      "dark": theme === themeModeEnum.darkMode, 
-      "light" : theme === themeModeEnum.lightMode  
-      })}> 
-      <Header />
-      <CardList/>
-    </main>
+    <section className="container">
+      <CardList />
+    </section>
   )
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,26 @@
+import classNames from "classnames";
+import { Header } from "components";
 import { CardSelected, Home } from "pages";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useThemeMode } from "state/hooks/useThemeMode";
+import { themeModeEnum } from "utils/enum/theme-mode-enum";
+import "../theme.scss";
 
 export default function AppRouter() {
+    const theme = useThemeMode(); 
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/*" element={<h1>Page Not Found</h1>} />
-                <Route path="/cardSelected" element={<CardSelected/>}/>
-            </Routes>
-        </Router>
+        <main className={classNames({
+            "dark": theme === themeModeEnum.darkMode,
+            "light": theme === themeModeEnum.lightMode
+         })}>
+            <Header />
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/*" element={<h1>Page Not Found</h1>} />
+                    <Route path="/cardSelected" element={<CardSelected />} />
+                </Routes>
+            </Router>
+        </main>
     )
 }
