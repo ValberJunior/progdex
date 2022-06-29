395e1bdc42053ad5cb39251144b20cb143dd1aa6
ValberJunior <valberjr1992@gmail.com>
2022-06-29 01:19:43 -0300

    toggle light/dark mode

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,32 @@
-import { FaMoon } from "react-icons/fa";
+import { useState } from "react";
+import { FaMoon , FaSun } from "react-icons/fa";
+import { useSetRecoilState } from "recoil";
+import { themeMode } from "state/ atom";
+import { useThemeMode } from "state/hooks/useThemeMode";
+import { themeModeEnum } from "utils/enum/theme-mode-enum";
 import style from "./header.module.scss";
 
-
 const Header = () => {
+
+  const theme = useThemeMode(); 
+  const setTheme = useSetRecoilState(themeMode);
+  const [ darkMode, setDarkMode ] = useState(false);
+
+  const selectThemeMode = ()=>{
+       setTheme(theme !== themeModeEnum.darkMode ? themeModeEnum.darkMode : themeModeEnum.lightMode);
+       if(theme === themeModeEnum.darkMode) setDarkMode(true);
+       else setDarkMode(false);
+  }
+
   return (
     <header className={style.container}>
       <section className={style.container__wrapper}>
           <h1 className={style["container__wrapper-title"]}>ProgDex</h1>
-          <FaMoon className={style["container__wrapper-icon"]}/>
+          <span onClick={selectThemeMode}>
+            { darkMode ? (<FaMoon className={style["container__wrapper-icon"]} />)
+              : (<FaSun className={style["container__wrapper-icon"]} />)
+             }
+          </span>
       </section>
     </header>
   )
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,18 @@
 import { Header } from 'components'
 import style from "./home.module.scss";
+import classNames from 'classnames';
+import { themeModeEnum } from 'utils/enum/theme-mode-enum';
+import { useThemeMode } from 'state/hooks/useThemeMode';
 
 const Home = () => {
 
+  const theme = useThemeMode(); 
+  
   return (
-    <main className={style.container}>
+    <main className={classNames({
+      [style.dark]: theme === themeModeEnum.darkMode, 
+      [style.light] : theme === themeModeEnum.lightMode  
+      })}> 
       <Header />
     </main>
   )
