879299d1f7129c7347c83fa1fb24b00f4de9a74c
ValberJunior <valberjr1992@gmail.com>
2022-07-01 01:22:12 -0300

    button scroll to top

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,40 @@
 import { Loading } from 'components';
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useState } from 'react';
+import { FaAngleUp } from 'react-icons/fa';
 import "./_Home.scss";
 
 const CardList = lazy(() => import('components/cardList/CardList'))
+//scroll function
+const scrollToTop = () =>{
+  window.scrollTo({
+    top: 0, 
+    behavior: 'smooth'
+  }); 
+};
 
 const Home = () => {
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 800) return setVisible(true);
+    if (scrolled <= 800) return setVisible(false)
+  };
+  window.addEventListener('scroll', toggleVisible);
 
   return (
     <section className="container">
        <Suspense fallback={<Loading/>}>
           <CardList />
         </Suspense>
+        <button
+         onClick={scrollToTop}
+         style={{display: visible ? 'inline' : 'none'}}
+         className="container__top"
+         >
+          <FaAngleUp/>
+        </button>
     </section>
   )
 }
